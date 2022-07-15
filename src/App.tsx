114b25bf5d6c477617a7184f02a43e6b23fa083d
Emerson Herrera Diaz <emrahedi@live.com>
2022-07-15 09:44:19 -0500

    privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate  } from 'react-router-dom';
+//import { Redirect  } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard";
 import "./App.scss";
 function App() {
+  const reload = () => window.location.reload();
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/privacy-policy" element={<Navigate replace={true} to="/privacy-policy.html" />}/> 
+          
         </Routes>
       </Router>
     </div>
