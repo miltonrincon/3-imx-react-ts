b761a670b7055a2aca844a669c92fbda934a341a
Oleh Pshyk <opshyk@apple.com>
2022-07-17 15:58:01 +0300

    user context and useHooks

--- src/components/Accbox/Accbox.tsx
+++ src/components/Accbox/Accbox.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import React, { useState, useContext } from 'react';
+// import { useUser } from 'context/user-context';
 import "./Accbox.scss"
 const Accbox = () => {
-  // get accountName from Context/Redux
+  // const {state:user} = useUser();
   const [accountName] = useState('0xkdjdsd');
   return (
     <div className="Accbox">
+      {/* <div className="acc-name">{user?.userName}</div> */}
       <div className="acc-name">{accountName}</div>
       <div className="acc-img-container">
         <img className="acc-image" src="/user.png" alt="acc" />
