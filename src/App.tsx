e90049d0ab8308e99bce88e373a7cab0265e9016
Oleh <pshyk.oleg@gmail.com>
2022-07-18 17:42:17 +0300

    User context (#6)
    * email modal
    
    * dropdowns UI
    
    * dropdowns and grbuttons and copytexttocontext done 100%
    
    * some small fixes
    
    * prev fixes
    
    * user context and useHooks
    
    * reducer userfields
    
    * async data action instruction
    
    Co-authored-by: Oleh Pshyk <opshyk@apple.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserProvider } from 'context/user-context';
 import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard";
 import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </UserProvider>
     </div>
   );
 }
