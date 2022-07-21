ac158fddf30e7a38b1aefeb2c80168433be8d2c7
Oleh Pshyk <opshyk@apple.com>
2022-07-21 10:16:45 +0300

    toastify with custom styles

--- src/components/Dashboard/DashboardHome/DashboardHome.tsx
+++ src/components/Dashboard/DashboardHome/DashboardHome.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 import SpiredSlider from "components/SpiredSlider/SpiredSlider";
 import Grbutton from "components/Grbutton/Grbutton";
 import HomeListItem from "./HomeListItem/HomeListItem";
@@ -30,51 +31,62 @@ const DashboardHome = () => {
   //   setListDataArr([....])
   // }, []);
   
+  const notify = () => toast("Notify text!");
+  const notifys = () => toast.success("Notify success text!");
+  const notifyi = () => toast.info("Notify info text!");
+  const notifye= () => toast.error("Notify error text!");
+
   return (
     <React.Fragment>
-    
-    <div className="DashboardHome">
-      <div className="l-container">
-        <div className="top-container">
-          <div className="top-title">
-            Funky Feed
+      <div style={{position:'absolute'}}>
+        <button onClick={notify}>Notify text!</button>
+        <button onClick={notifys}>Notify success!</button>
+        <button onClick={notifyi}>Notify info!</button>
+        <button onClick={notifye}>Notify error!</button>
+      </div>
+
+      <div className="DashboardHome">
+        <div className="l-container">
+          <div className="top-container">
+            <div className="top-title">
+              Funky Feed
+            </div>
+            <Grbutton
+              onClick={()=>{}}
+            >
+              Earn Points Faster
+            </Grbutton>
           </div>
-          <Grbutton
-            onClick={()=>{}}
-          >
-            Earn Points Faster
-          </Grbutton>
-        </div>
-        <div className="l-body">
-          <div className="anime-img-container">
-            <img className="anime-img" src="/talking.gif" alt="talking"/>
+          <div className="l-body">
+            <div className="anime-img-container">
+              <img className="anime-img" src="/talking.gif" alt="talking"/>
+            </div>
+          </div>
+          <div className="l-list">
+            <Scrollbars style={{ height: 345 }}>
+              {listDataArr.map(el=>(
+                <HomeListItem 
+                  key = {el.id}
+                  data = {el}
+                />
+              ))}
+            </Scrollbars>
           </div>
         </div>
-        <div className="l-list">
-          <Scrollbars style={{ height: 345 }}>
-            {listDataArr.map(el=>(
-              <HomeListItem 
-                key = {el.id}
-                data = {el}
-              />
-            ))}
-          </Scrollbars>
+        <div className="r-container">
+          <div className="top-container">
+            <div className="top-title">
+              Mojo Multipliers
+            </div>
+            <Grbutton
+              onClick={()=>{}}
+            >
+              View All Mojo Vials
+            </Grbutton>
+          </div>
+          <SpiredSlider/>
         </div>
       </div>
-      <div className="r-container">
-        <div className="top-container">
-          <div className="top-title">
-            Mojo Multipliers
-          </div>
-          <Grbutton
-            onClick={()=>{}}
-          >
-            View All Mojo Vials
-          </Grbutton>
-        </div>
-        <SpiredSlider/>
-      </div>
-    </div>
     </React.Fragment>
   )
 }
