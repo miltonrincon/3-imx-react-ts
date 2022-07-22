10f684964cb10b7270f5d94a93f9c5422b2ab10b
Oleh Pshyk <opshyk@apple.com>
2022-07-23 00:38:20 +0300

    some redirects and routings

--- src/routes/dashboardRoutes.ts
+++ src/routes/dashboardRoutes.ts
@@ -1,12 +1,12 @@
 import { ReactComponent as DashHomeIcon } from "assets/icons/dash_nav_home.svg";
 import { ReactComponent as DashInventoryIcon } from "assets/icons/dash_nav_inventory.svg";
 import { ReactComponent as DashQuestIcon } from "assets/icons/dash_nav_quest.svg";
-import { ReactComponent as DashVialIcon } from "assets/icons/dash_nav_vial.svg";
+import { ReactComponent as DashMintIcon } from "assets/icons/dash_nav_mint.svg";
 import { ReactComponent as DashRewardsIcon } from "assets/icons/dash_nav_rewards.svg";
 import { ReactComponent as DashInvitesIcon } from "assets/icons/dash_nav_invites.svg";
 
 import DashboardHome from 'components/Dashboard/DashboardHome/DashboardHome';
-import DashboardVial from 'components/Dashboard/DashboardVial/DashboardVial';
+import DashboardMint from 'components/Dashboard/DashboardMint/DashboardMint';
 import DashboardQuest from 'components/Dashboard/DashboardQuest/DashboardQuest';
 import DashboardInventory from 'components/Dashboard/DashboardInventory/DashboardInventory';
 import DashboardRewards from 'components/Dashboard/DashboardRewards/DashboardRewards';
@@ -21,10 +21,10 @@ export const dashRoutes = [
     component: DashboardHome
   },
   {
-    to: '/vial',
-    iconComponent: DashVialIcon,
-    label: 'vial',
-    component: DashboardVial
+    to: '/mint',
+    iconComponent: DashMintIcon,
+    label: 'mint',
+    component: DashboardMint
   },
   {
     to: '/quest',
